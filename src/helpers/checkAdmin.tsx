ae332bf677cf4b8a7c5ff5002d04f42ff670ff50
Timur Bykov <bikov.timur.013@gmail.com>
2023-04-04 23:55:40 +0300

    CheckAdmin fix

--- src/helpers/checkAdmin.tsx
+++ src/helpers/checkAdmin.tsx
@@ -5,7 +5,7 @@ export default async function checkAdmin() {
   // TODO: подрубить в проект redux toolkit чтобы брать значение что ты админ из стейта
   //заодно протестить возможно ли прокидывать стейт во вспомогательные функции
 
-  let { authorized } = await store.getState();
+  let { authorized } = store.getState();
 
   // console.log()
   return true;
