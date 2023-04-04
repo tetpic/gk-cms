f4f98a3ab7e8a58ea53ac294d8e54044361d4103
Timur Bykov <bikov.timur.013@gmail.com>
2023-04-04 23:37:31 +0300

    authentication

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,38 +1,26 @@
-"use client"
-import { useRouter } from "next/navigation"
-import AdminNavigation from "./adminNavigation"
-import checkAdmin from "@/helpers/checkAdmin"
-import { useEffect } from "react"
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import AdminNavigation from './adminNavigation';
+import checkAdmin from '@/helpers/checkAdmin';
 
-
-
-
-export default function AdminMainLayout
-({
-    children,
-  }: {
-    children: React.ReactNode
-}) 
-{   
-  if(checkAdmin()) {  
+export default function AdminMainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  if (checkAdmin()) {
     useEffect(() => {
-
-      require('bootstrap/dist/js/bootstrap') 
-  }, [])  
-      return (
-        <div className='row'>     
-        <AdminNavigation/>
-        <section className='col-9'>
-          {children}
-        </section>
-      </div> 
-    )
+      require('bootstrap/dist/js/bootstrap');
+    }, []);
+    return (
+      <div className="row">
+        <AdminNavigation />
+        <section className="col-9">{children}</section>
+      </div>
+    );
   } else {
-      const router = useRouter()
-      router.push('/')
-    }
-       
+    const router = useRouter();
+    router.push('/');
+  }
 }
-
-
-  
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,25 +1,28 @@
-"use client"
-import { Action, AnyAction, ThunkAction, combineReducers, configureStore } from "@reduxjs/toolkit";
-import authReducer  from "./authUserSlice";
-import productsReducer, { ProductsInitialState } from "./productsSlice";
+'use client';
+import {
+  Action,
+  AnyAction,
+  ThunkAction,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
+import authReducer from './authUserSlice';
+import productsReducer, { ProductsInitialState } from './productsSlice';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 
-
-// config the store 
+// config the store
 const store = configureStore({
-    reducer: {
-       products: productsReducer,
-       authorized: authReducer 
-    }
- })
-
- 
+  reducer: {
+    products: productsReducer,
+    authorized: authReducer,
+  },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // export type IRootState = ReturnType<typeof store>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -32,5 +35,5 @@ const makeStore = () => store;
 
 export const wrapper = createWrapper(makeStore);
 
-// export default the store 
-export default store
+// export default the store
+export default store;
